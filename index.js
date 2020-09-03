e017c73de1c89fad641b5392e1ae97327da2b6ff
sbarrack <12619603+sbarrack@users.noreply.github.com>
2020-09-02 21:58:22 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Master Skribbl Script
+// @namespace    https://github.com/sbarrack/skribbl-community-scripts/
+// @version      0.1
+// @description  Collected and reworked Skribbl scripts
+// @author       sbarrack
+// @license      none
+// @match        http*://skribbl.io/*
+// @grant        none
+// ==/UserScript==
+/* jshint esversion: 6 */
+
+(function() {
+    'use strict';
+
+
+    // function sendMessage() {
+    //     var request = new XMLHttpRequest();
+    //     request.open("POST", "https://discordapp.com/api/webhooks/676118118082281513/ZS5YcWhurzokBrKX9NgexqtxrJA5Pu2Bo4i7_JsIxC-JIbPBVhSZkcVVukGOro52rnQA");
+  
+    //     request.setRequestHeader('Content-type', 'application/json');
+  
+    //     var params = {
+    //       username: "My Webhook Name",
+    //       avatar_url: "",
+    //       content: "The message to send"
+    //     }
+  
+    //     request.send(JSON.stringify(params));
+    //   }
+})();
