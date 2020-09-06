9e55f2ad6e5198d30a55945b685146dd76914b4d
sbarrack <12619603+sbarrack@users.noreply.github.com>
2020-09-06 16:30:56 -0700

    update version

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Master Skribbl Script
 // @namespace    https://github.com/sbarrack/skribbl-community-scripts/
-// @version      0.4
+// @version      0.5
 // @description  Collected and reworked Skribbl scripts
 // @author       sbarrack
 // @license      none
