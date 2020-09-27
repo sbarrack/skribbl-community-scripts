ccfd89cfd5adfa22cc42779c07e4b43e54fdd723
sbarrack <12619603+sbarrack@users.noreply.github.com>
2020-09-27 13:55:01 -0700

    version

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Master Skribbl Script
 // @namespace    https://github.com/sbarrack/skribbl-community-scripts/
-// @version      0.11
+// @version      0.12
 // @description  Collected and reworked Skribbl scripts
 // @author       sbarrack
 // @match        http*://skribbl.io/*
